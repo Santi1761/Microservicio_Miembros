624f6ff50a7a2bd4358139da2e92ed8141a58a79
Santiago Arboleda Velasco <santiagoarboledavelasco@gmail.com>
2025-03-15 17:57:41 -0500

    feat: implement RabbitMQ for Miembros

--- src/main/java/co/analisys/gimnasio/miembros/service/MiembroService.java
+++ src/main/java/co/analisys/gimnasio/miembros/service/MiembroService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 @Service
 public class MiembroService implements IMiembroService {
@@ -15,20 +16,26 @@ public class MiembroService implements IMiembroService {
     @Autowired
     private MiembroRepository miembroRepository;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Override
     public MiembroDTO registrar(MiembroDTO miembroDTO) {
+
         Miembro miembro = new Miembro(new MiembroID(miembroDTO.getId()),
                 miembroDTO.getNombre(),
                 miembroDTO.getEmail(),
                 miembroDTO.getFechaInscripcion()
         );
-        miembro = miembroRepository.save(miembro);
-        return new MiembroDTO(
-                miembro.getId().getId(),
-                miembro.getNombre(),
-                miembro.getEmail(),
-                miembro.getFechaInscripcion()
+        miembroRepository.save(miembro);
+
+        // Se envia notificación a RabbitMQ
+        rabbitTemplate.convertAndSend(
+                "gimnasio.exchange",
+                "inscripcion.nueva",
+                miembroDTO
         );
+        return miembroDTO;
     }
 
     @Override
