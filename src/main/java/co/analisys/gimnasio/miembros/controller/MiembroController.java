3facd33d9b7bd773482aa780a4527d528c77f5dd
Dantesiio <DavidDo29@hotmail.com>
2025-04-21 17:13:57 -0500

    fix to apigateway

--- src/main/java/co/analisys/gimnasio/miembros/controller/MiembroController.java
+++ src/main/java/co/analisys/gimnasio/miembros/controller/MiembroController.java
@@ -4,6 +4,7 @@ import co.analisys.gimnasio.miembros.dto.MiembroDTO;
 import co.analisys.gimnasio.miembros.service.IMiembroService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
